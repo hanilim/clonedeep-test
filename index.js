a87289ebf3088a04f5bcbc40ebd7c2c01cdf3543
hanilim <hanilim@gmail.com>
2018-08-17 09:06:25 -0400

    this is minimum to get error to appear

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 Debugging error of clonedeep + --optimize-minimize + source-map in Webpack 3
 */
 import clonedeep from 'lodash.clonedeep';
-import _ from 'lodash';
 
 const obj = {
   foo: 'bar'
@@ -10,5 +9,3 @@ const obj = {
 
 const obj2 = clonedeep(obj);
 console.log(obj2);
-const obj3 = _.cloneDeep(obj);
-console.log(obj3);
