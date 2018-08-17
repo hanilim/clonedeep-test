7f9ce7a60a3773efc8c834dc93664109ed9e89fa
hanilim <hanilim@gmail.com>
2018-08-17 10:55:27 -0400

    more tests

--- deep-copy.js
+++ deep-copy.js
@@ -5,10 +5,15 @@ Debugging error of clonedeep + --optimize-minimize + source-map in Webpack 3
 import deepcopy from 'deep-copy';
 
 const obj = {
-  foo: 'bar'
+  foo: 'bar',
+  bar: {
+    baz: 1
+  }
 };
 
 const obj2 = deepcopy(obj);
 obj2.foo = 'baz';
+obj2.bar.baz = 2;
 
+console.log(obj);
 console.log(obj2);
